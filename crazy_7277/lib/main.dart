55ed51915217e718d2ab678b92157f5bc4d51597
network7277 <husanjurayev7277@gmail.com>
2023-04-10 23:26:19 +0500

    icon

--- crazy_7277/lib/main.dart
+++ crazy_7277/lib/main.dart
@@ -1,71 +1,199 @@
 import 'package:flutter/material.dart';
+
 void main(){
   runApp(
-    Container(
-      color: Color.fromARGB(0, 24, 21, 23),
-      child: Container(
-        margin: EdgeInsets.fromLTRB(
-          50, 
-          220, 
-          50, 
-          220,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Color.fromARGB(255, 17, 6, 222),
-                Color.fromARGB(255, 191, 20, 8),
-              ],
-            ),
-            border: Border.all(
-              color: Colors.white,
-              width: 2,
-              style: BorderStyle.solid,
-            ),
-          ),
-          child: Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(
-                width: 20,
-                color: Colors.white,
-                style: BorderStyle.solid,
-              ),
-            ),
-            child: Container(
-              margin: EdgeInsets.fromLTRB(
-                30, 
-                60,
-                30, 
-                60,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                  border: Border.all(
-                    width: 15,
-                    color: Colors.white,
-                    style: BorderStyle.solid,
-                  ), 
-                ),
-                child: Container(
-                  alignment: Alignment(2, -5),
-                  child: Text(
-                    'o',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      fontSize: 60,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                    ),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 90,
+                  height: 150,
+                  child: Icon(
+                    Icons.language,
+                    color: Colors.blueAccent,
+                    size: 70,
                   ),
                 ),
+                Text(
+                'Language',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 20,
+                ),
+                ),
+                Container(
+                  width: 100,
+                  height: 20,
+                  color: Colors.white,
+                ),
+                Container(
+                  width: 100,
+                  height: 150,
+                  child: Icon(
+                    Icons.translate,
+                    color: Colors.lightBlue,
+                    size: 70,
+                  ),
+                ),
+                Text(
+                  'Translate',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 20,
+                  color: Colors.white,
+                ),
+                Container(
+                  width: 100,
+                  height: 150,
+                  child: Icon(
+                    Icons.link,
+                    color: Colors.lightBlueAccent,
+                    size: 70,
+                  ),
+                ),
+                Text(
+                  'Link',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20
+                  ),
+                ),
+              ],
             ),
-          ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.forum,
+                      color: Colors.lightGreenAccent,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Forum',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.mark_as_unread_rounded,
+                      color: Colors.greenAccent,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Mark unread chat ALT',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.unsubscribe,
+                      color: Colors.lightGreen,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Unsubscribe',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.photo,
+                      color: Colors.redAccent,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Photo',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.camera,
+                      color: Colors.pinkAccent,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Camera',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 150,
+                    child: Icon(
+                      Icons.flash_auto,
+                      color: Colors.pink,
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    'Flash',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ), 
+            ),
+          ],
+        ),
       ),
-    ) 
+    ),
   );
 }
